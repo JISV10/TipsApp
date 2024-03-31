a8802571d8ec925df4b0272d52771e5a53210f3a
JISV10 <jgsaez@uc.cl>
2024-03-31 00:37:42 -0600

    Added Cards display and List of data

---
+++ app/src/main/java/com/example/tipsapp/model/Agrocards.kt
@@ -0,0 +1,13 @@
+package com.example.tipsapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Agrocards(
+
+    val tipNumber: Int,
+    @StringRes val title: Int,
+    @StringRes val description: Int,
+    @DrawableRes val image: Int
+
+)
